3223b0c6e9fe7bf6c581e7d9b74c47ea204585bc
tczyz <tomczyz97@gmail.com>
2024-03-02 22:55:24 +0100

    Add basic scene setup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
+mod models;
+
 use bevy::prelude::*;
+use bevy::math::primitives::Capsule3d;
 use leaprs::{Connection, ConnectionConfig, Event};
+
+
+/// Struct to mark SpatialBundle, which is a parent of all [`BoneComponent`]s.
+/// You can use it for to change relative Transform of all digits at once.
+#[derive(Component)]
+pub struct HandsOrigin;
+
+#[derive(Component)]
+pub struct BoneComponent;
+
+pub const CAMERA_ORIGIN: Transform = Transform::from_xyz(0., 350., 500.);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::DARK_GRAY))
+        .add_systems(Startup, (spawn_light, spawn_camera))
+        .add_systems(Startup, setup)
         .add_systems(Startup, create_connection)
         .add_systems(Update, update_hand_data)
         .run();
@@ -27,4 +44,52 @@ fn update_hand_data(mut leap_conn: NonSendMut<Connection>) {
             _ => {}
         }
     }
+}
+
+fn spawn_light(mut commands: Commands) {
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            intensity: 10_000_000.,
+            range: 100.0,
+            ..default()
+        },
+        transform: Transform::from_xyz(8.0, 16.0, 8.0),
+        ..default()
+    });
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: CAMERA_ORIGIN.looking_at(Vec3::Y * 200., Vec3::Y),
+            ..default()
+        }
+    ));
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let debug_material = materials.add(StandardMaterial { ..default() });
+    let capsule = Capsule3d::new(50., 100.);
+
+    commands
+        .spawn((SpatialBundle::default(), HandsOrigin))
+        .with_children(|parent| for _ in 0..40 {
+            parent
+                .spawn((PbrBundle {
+                    mesh: meshes.add(capsule),
+                    visibility: Visibility::Visible,
+                    material: debug_material.clone(),
+                    transform: Transform::from_xyz(
+                        120.0,
+                        2.0,
+                        0.0,
+                    ),
+                    ..default()
+                }, BoneComponent));
+        });
 }
