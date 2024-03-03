a2c71a2557530d56d95ce8c366be3308bcbd4db7
tczyz <tomczyz97@gmail.com>
2024-03-03 21:59:12 +0100

    Draw hands using LeapC data

---
+++ README.md
@@ -0,0 +1,5 @@
+## Progress
+
+### Initial setup
+
+<video src="assets/VID_20240303_214659.mp4" width="320" height="240" controls></video>
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::f32::consts::PI;
 use bevy::prelude::*;
 use bevy::math::primitives::Capsule3d;
-use leaprs::{Connection, ConnectionConfig, Event};
+use leaprs::{Bone, Connection, ConnectionConfig, Digit, Event};
 
 
 /// Struct to mark SpatialBundle, which is a parent of all [`BoneComponent`]s.
@@ -16,7 +17,7 @@ pub const CAMERA_ORIGIN: Transform = Transform::from_xyz(0., 350., 500.);
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::DARK_GRAY))
+        .insert_resource(ClearColor(Color::SEA_GREEN))
         .add_systems(Startup, (spawn_light, spawn_camera))
         .add_systems(Startup, setup)
         .add_systems(Startup, create_connection)
@@ -31,17 +32,49 @@ fn create_connection(world: &mut World) {
     world.insert_non_send_resource(connection);
 }
 
-fn update_hand_data(mut leap_conn: NonSendMut<Connection>) {
-    if let Ok(message) = leap_conn.poll(25) {
+fn update_hand_data(
+    mut leap_conn: NonSendMut<Connection>,
+    mut digits_query: Query<(&mut Transform, &mut Visibility), With<BoneComponent>>,
+) {
+    if let Ok(message) = leap_conn.poll(50) {
         match &message.event() {
             Event::Connection(_) => println!("connection event"),
             Event::Device(_) => println!("device event"),
-            Event::Tracking(e) => if let Some(hand) = e.hands().first() {
-                println!("{} hand(s)", hand.pinch_strength());
-            },
+            Event::Tracking(e) => {
+                let mut query_iter = digits_query.iter_mut();
+
+                for hand in e.hands().iter() {
+                    for digit in hand.digits().iter() {
+                        for bone in get_bones(digit) {
+                            let (mut transform, mut visibility) = query_iter.next().unwrap();
+
+                            *transform = Transform {
+                                translation: Vec3::from_array(bone.prev_joint().array()),
+                                rotation: Quat::from_array(bone.rotation().array()) * Quat::from_rotation_x(PI / 2.),
+                                ..default()
+                            };
+                            *visibility = Visibility::Visible;
+                        }
+                    }
+                }
+
+                // hide elements if hand data is unavailable
+                while let Some((_, mut visibility)) = query_iter.next() {
+                    *visibility = Visibility::Hidden;
+                }
+            }
             _ => {}
         }
     }
+}
+
+fn get_bones<'a>(digit: &'a Digit<'a>) -> [Bone<'a>; 4] {
+    [
+        digit.distal(),
+        digit.proximal(),
+        digit.intermediate(),
+        digit.metacarpal(),
+    ]
 }
 
 fn spawn_light(mut commands: Commands) {
@@ -72,7 +105,7 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let debug_material = materials.add(StandardMaterial { ..default() });
-    let capsule = Capsule3d::new(50., 100.);
+    let capsule = Capsule3d::new(7., 25.);
 
     commands
         .spawn((SpatialBundle::default(), HandsOrigin))
