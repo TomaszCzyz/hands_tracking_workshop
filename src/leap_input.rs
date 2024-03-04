21feead36ed190ab7b523f7d6a933b98c463063e
tczyz <tomczyz97@gmail.com>
2024-03-04 23:06:47 +0100

    Add pinch events

--- src/leap_input.rs
+++ src/leap_input.rs
@@ -5,8 +5,14 @@ use bevy::asset::Assets;
 use bevy::hierarchy::BuildChildren;
 use bevy::math::{Quat, Vec3};
 use bevy::pbr::{PbrBundle, StandardMaterial};
-use bevy::prelude::{Capsule3d, Commands, Component, default, Mesh, NonSendMut, Query, ResMut, SpatialBundle, Transform, Visibility, With, World};
-use leaprs::{Bone, Connection, ConnectionConfig, Digit, Event};
+use bevy::prelude::{Capsule3d, Commands, Component, default, Event, EventWriter, Mesh, NonSendMut, Query, ResMut, SpatialBundle, Transform, Visibility, With, World};
+use leaprs::{Bone, Connection, ConnectionConfig, Digit, Event as LeapEvent, HandType};
+
+#[derive(Event, Debug, Clone)]
+pub struct HandPinch {
+    pub hand_type: HandType,
+    pub pos: Vec3,
+}
 
 /// Struct to mark SpatialBundle, which is a parent of all [`BoneComponent`]s.
 /// You can use it for to change relative Transform of all digits at once.
@@ -20,7 +26,9 @@ pub struct LeapInputPlugin;
 
 impl Plugin for LeapInputPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, create_connection)
+        app
+            .add_event::<HandPinch>()
+            .add_systems(Startup, create_connection)
             .add_systems(Startup, setup)
             .add_systems(Update, update_hand_data);
     }
@@ -62,15 +70,23 @@ fn create_connection(world: &mut World) {
 fn update_hand_data(
     mut leap_conn: NonSendMut<Connection>,
     mut digits_query: Query<(&mut Transform, &mut Visibility), With<BoneComponent>>,
+    mut hand_pinch: EventWriter<HandPinch>,
 ) {
     if let Ok(message) = leap_conn.poll(50) {
         match &message.event() {
-            Event::Connection(_) => println!("connection event"),
-            Event::Device(_) => println!("device event"),
-            Event::Tracking(e) => {
+            LeapEvent::Connection(_) => println!("connection event"),
+            LeapEvent::Device(_) => println!("device event"),
+            LeapEvent::Tracking(e) => {
                 let mut query_iter = digits_query.iter_mut();
 
                 for hand in e.hands().iter() {
+                    if hand.pinch_strength() > 0.7 {
+                        hand_pinch.send(HandPinch {
+                            hand_type: hand.hand_type(),
+                            pos: Vec3::from_array(hand.index().distal().next_joint().array()),
+                        });
+                    }
+
                     for digit in hand.digits().iter() {
                         for bone in get_bones(digit) {
                             let (mut transform, mut visibility) = query_iter.next().unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::leap_input::LeapInputPlugin;
+use crate::leap_input::{HandPinch, LeapInputPlugin};
 
 mod leap_input;
 
@@ -11,6 +11,7 @@ fn main() {
         .add_plugins((DefaultPlugins, LeapInputPlugin))
         .insert_resource(ClearColor(Color::SEA_GREEN))
         .add_systems(Startup, (spawn_light, spawn_camera))
+        .add_systems(Update, spawn_on_pinch)
         .run();
 }
 
@@ -18,13 +19,18 @@ fn spawn_light(mut commands: Commands) {
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             shadows_enabled: true,
-            intensity: 10_000_000.,
-            range: 100.0,
+            intensity: 600000.,
+            range: 20000000.,
             ..default()
         },
-        transform: Transform::from_xyz(8.0, 16.0, 8.0),
+        transform: Transform::from_xyz(107.60522, 235.05785, 32.378628),
         ..default()
     });
+
+    // commands.insert_resource(AmbientLight {
+    //     color: Color::ORANGE_RED,
+    //     brightness: 0.02,
+    // });
 }
 
 fn spawn_camera(mut commands: Commands) {
@@ -34,5 +40,26 @@ fn spawn_camera(mut commands: Commands) {
             ..default()
         }
     );
+}
+
+fn spawn_on_pinch(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut right_pinch_events: EventReader<HandPinch>,
+) {
+    if let Some(event) = right_pinch_events.read().next() {
+        let debug_material = materials.add(StandardMaterial { lightmap_exposure: 0.5, base_color: Color::CYAN, metallic: 0.8, ..default() });
+
+        println!("spawning circle in position: {}", event.pos);
+        commands
+            .spawn(PbrBundle {
+                mesh: meshes.add(Sphere::default().mesh().uv(32, 18).scaled_by(Vec3::splat(15f32))),
+                visibility: Visibility::Visible,
+                material: debug_material, // materials.add(Color::WHITE),
+                transform: Transform::from_translation(event.pos),
+                ..default()
+            });
+    }
 }
 
