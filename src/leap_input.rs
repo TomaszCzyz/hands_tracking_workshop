c2f55f88fdfc733f28e749550e6d024fafd3ec6d
tczyz <tomczyz97@gmail.com>
2024-03-04 21:40:10 +0100

    Extract Leap related stuff to plugin

---
+++ src/leap_input.rs
@@ -0,0 +1,106 @@
+use std::f32::consts::PI;
+
+use bevy::app::{App, Plugin, Startup, Update};
+use bevy::asset::Assets;
+use bevy::hierarchy::BuildChildren;
+use bevy::math::{Quat, Vec3};
+use bevy::pbr::{PbrBundle, StandardMaterial};
+use bevy::prelude::{Capsule3d, Commands, Component, default, Mesh, NonSendMut, Query, ResMut, SpatialBundle, Transform, Visibility, With, World};
+use leaprs::{Bone, Connection, ConnectionConfig, Digit, Event};
+
+/// Struct to mark SpatialBundle, which is a parent of all [`BoneComponent`]s.
+/// You can use it for to change relative Transform of all digits at once.
+#[derive(Component)]
+pub struct HandsOrigin;
+
+#[derive(Component)]
+pub struct BoneComponent;
+
+pub struct LeapInputPlugin;
+
+impl Plugin for LeapInputPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, create_connection)
+            .add_systems(Startup, setup)
+            .add_systems(Update, update_hand_data);
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let debug_material = materials.add(StandardMaterial { ..default() });
+    let capsule = Capsule3d::new(7., 25.);
+
+    commands
+        .spawn((SpatialBundle::default(), HandsOrigin))
+        .with_children(|parent| for _ in 0..40 {
+            parent
+                .spawn((PbrBundle {
+                    mesh: meshes.add(capsule),
+                    visibility: Visibility::Visible,
+                    material: debug_material.clone(),
+                    transform: Transform::from_xyz(
+                        120.0,
+                        2.0,
+                        0.0,
+                    ),
+                    ..default()
+                }, BoneComponent));
+        });
+}
+
+fn create_connection(world: &mut World) {
+    let mut connection = Connection::create(ConnectionConfig::default()).expect("Failed to create connection");
+    connection.open().expect("Failed to open the connection");
+
+    world.insert_non_send_resource(connection);
+}
+
+fn update_hand_data(
+    mut leap_conn: NonSendMut<Connection>,
+    mut digits_query: Query<(&mut Transform, &mut Visibility), With<BoneComponent>>,
+) {
+    if let Ok(message) = leap_conn.poll(50) {
+        match &message.event() {
+            Event::Connection(_) => println!("connection event"),
+            Event::Device(_) => println!("device event"),
+            Event::Tracking(e) => {
+                let mut query_iter = digits_query.iter_mut();
+
+                for hand in e.hands().iter() {
+                    for digit in hand.digits().iter() {
+                        for bone in get_bones(digit) {
+                            let (mut transform, mut visibility) = query_iter.next().unwrap();
+
+                            *transform = Transform {
+                                translation: Vec3::from_array(bone.prev_joint().array()),
+                                rotation: Quat::from_array(bone.rotation().array()) * Quat::from_rotation_x(PI / 2.),
+                                ..default()
+                            };
+                            *visibility = Visibility::Visible;
+                        }
+                    }
+                }
+
+                // hide elements if hand data is unavailable
+                while let Some((_, mut visibility)) = query_iter.next() {
+                    *visibility = Visibility::Hidden;
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
+fn get_bones<'a>(digit: &'a Digit<'a>) -> [Bone<'a>; 4] {
+    [
+        digit.distal(),
+        digit.proximal(),
+        digit.intermediate(),
+        digit.metacarpal(),
+    ]
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,22 @@
-use std::f32::consts::PI;
 use bevy::prelude::*;
-use bevy::math::primitives::Capsule3d;
-use leaprs::{Bone, Connection, ConnectionConfig, Digit, Event};
 
+use crate::leap_input::LeapInputPlugin;
 
-/// Struct to mark SpatialBundle, which is a parent of all [`BoneComponent`]s.
-/// You can use it for to change relative Transform of all digits at once.
-#[derive(Component)]
-pub struct HandsOrigin;
-
-#[derive(Component)]
-pub struct BoneComponent;
+mod leap_input;
 
 pub const CAMERA_ORIGIN: Transform = Transform::from_xyz(0., 350., 500.);
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, LeapInputPlugin))
         .insert_resource(ClearColor(Color::SEA_GREEN))
         .add_systems(Startup, (spawn_light, spawn_camera))
-        .add_systems(Startup, setup)
-        .add_systems(Startup, create_connection)
-        .add_systems(Update, update_hand_data)
         .run();
 }
 
-fn create_connection(world: &mut World) {
-    let mut connection = Connection::create(ConnectionConfig::default()).expect("Failed to create connection");
-    connection.open().expect("Failed to open the connection");
-
-    world.insert_non_send_resource(connection);
-}
-
-fn update_hand_data(
-    mut leap_conn: NonSendMut<Connection>,
-    mut digits_query: Query<(&mut Transform, &mut Visibility), With<BoneComponent>>,
-) {
-    if let Ok(message) = leap_conn.poll(50) {
-        match &message.event() {
-            Event::Connection(_) => println!("connection event"),
-            Event::Device(_) => println!("device event"),
-            Event::Tracking(e) => {
-                let mut query_iter = digits_query.iter_mut();
-
-                for hand in e.hands().iter() {
-                    for digit in hand.digits().iter() {
-                        for bone in get_bones(digit) {
-                            let (mut transform, mut visibility) = query_iter.next().unwrap();
-
-                            *transform = Transform {
-                                translation: Vec3::from_array(bone.prev_joint().array()),
-                                rotation: Quat::from_array(bone.rotation().array()) * Quat::from_rotation_x(PI / 2.),
-                                ..default()
-                            };
-                            *visibility = Visibility::Visible;
-                        }
-                    }
-                }
-
-                // hide elements if hand data is unavailable
-                while let Some((_, mut visibility)) = query_iter.next() {
-                    *visibility = Visibility::Hidden;
-                }
-            }
-            _ => {}
-        }
-    }
-}
-
-fn get_bones<'a>(digit: &'a Digit<'a>) -> [Bone<'a>; 4] {
-    [
-        digit.distal(),
-        digit.proximal(),
-        digit.intermediate(),
-        digit.metacarpal(),
-    ]
-}
-
 fn spawn_light(mut commands: Commands) {
+    let t = 0;
+
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             shadows_enabled: true,
@@ -99,28 +38,3 @@ fn spawn_camera(mut commands: Commands) {
     );
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let debug_material = materials.add(StandardMaterial { ..default() });
-    let capsule = Capsule3d::new(7., 25.);
-
-    commands
-        .spawn((SpatialBundle::default(), HandsOrigin))
-        .with_children(|parent| for _ in 0..40 {
-            parent
-                .spawn((PbrBundle {
-                    mesh: meshes.add(capsule),
-                    visibility: Visibility::Visible,
-                    material: debug_material.clone(),
-                    transform: Transform::from_xyz(
-                        120.0,
-                        2.0,
-                        0.0,
-                    ),
-                    ..default()
-                }, BoneComponent));
-        });
-}
