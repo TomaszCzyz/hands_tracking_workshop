f1636f8f801c0fba2034dde3929ea5f2e3486b99
tczyz <tomczyz97@gmail.com>
2024-03-25 21:21:55 +0100

    Create simplified model of a hand

--- crates/hand_gestures/src/models.rs
+++ crates/hand_gestures/src/models.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::{Transform};
+use bevy::math::Vec3;
 
 pub enum HandType {
     Left,
@@ -9,24 +9,14 @@ pub enum HandType {
 pub struct HandData {
     /// Identifies the chirality of this hand.
     pub type_: HandType,
-
     /// How confident we are with a given hand pose.
     pub confidence: f32,
-
-    /// The distance between index finger and thumb.
-    pub pinch_distance: f32,
-
-    /// The average angle of fingers to palm.
-    pub grab_angle: f32,
-
-    /// The normalized estimate of the pinch pose.
-    /// Zero is not pinching; one is fully pinched.
-    pub pinch_strength: f32,
-
-    /// The normalized estimate of the grab hand pose.
-    /// Zero is not grabbing; one is fully grabbing.
-    pub grab_strength: f32,
-
-    /// The transform of the points between tops of the index finger and thumb.
-    pub pinch_transform: Transform,
+    pub thumb: Finger,
+    pub index: Finger,
+    pub middle: Finger,
+    pub ring: Finger,
+    pub pink: Finger,
 }
+
+/// Finger consist with 4 points: finger top, finger bottom and two joints in the middle.
+type Finger = [Vec3; 4];
